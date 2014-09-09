1cd67d1de3eb368466c49cac354395aae70436d7
Shreyas <rshreyas@zeomega.com>
2014-09-09 16:50:15 +0530

    Added code to use member data and show in table

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,21 +2,12 @@
 
 /* Controllers */
 
-var phonecatControllers = angular.module('phonecatControllers', []);
+var jivaMemberApp = angular.module('jivaMemberApp', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
+jivaMemberApp.controller('MemberListCtrl', function($scope, $http) {
+    $scope.name = "Shreyas";
+    $http.get('data/memberdata.json').success(function(data){
+      $scope.members = data;  
     });
+  });
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
