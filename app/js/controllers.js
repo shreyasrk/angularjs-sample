1f35625d940da87b31d7a6cbdc706f772485bc05
Shreyas <rshreyas@zeomega.com>
2014-09-12 19:14:35 +0530

    V0.0.1: Successful implementation for the Demo Project. Renders JSON data when searched for a field, on click of a button.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,14 +4,15 @@
 
 var memberAppControllers = angular.module('memberAppControllers', []);
 
-memberAppControllers.controller("MemberListCtrl", function($scope, $http) {
-    $scope.name = "Shreyas";
-    $http.get('data/memberdata.json').success(function(data) {
-		$scope.members = data;  
-	});
+memberAppControllers.controller("MemberListCtrl", ['$scope', '$http', '$filter', 
+		function($scope, $http, $filter) {
+		    $scope.name = "Shreyas";
+		    $http.get('data/memberdata.json').success(function(data) {
+				$scope.members = data;  
+			});
 
-    $scope.getMemberList = function getMemberList(fieldvalue) {
-   		return;
-    };
+		    $scope.getMemberList = function getMemberList(fieldvalue) {
+		   		$scope.filteredmembers = $filter('memberFirstNameExact')($scope.members, fieldvalue);
+		    };
 
-  });
+  }]);
