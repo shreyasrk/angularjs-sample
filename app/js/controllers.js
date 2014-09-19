22484b2bcc214a8328efb9e81936ddffb9ad3ff3
Shreyas <shrykulk.rk85@gmail.com>
2014-09-19 07:15:32 +0530

    Modified HTML to align to the center of the page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,13 +5,12 @@
 var memberAppControllers = angular.module('memberAppControllers', []);
 
 memberAppControllers.controller("MemberListCtrl", 
-	['$scope', '$filter', 'Member',
-	function($scope, $filter, Member) {
-	    $scope.name = "Shyam";
+        ['$scope', '$filter', 'Member', function($scope, $filter, Member) {
+            $scope.name = "Shreyas";
 	    $scope.members = Member.query();
 
 	    $scope.getMemberList = function getMemberList(fieldvalue) {
-	   		$scope.filteredmembers = $filter('filter')($scope.members, fieldvalue);
+                $scope.filteredmembers = $filter('filter')($scope.members, fieldvalue);
 	    };
 
   }]);
